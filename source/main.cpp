08ecad1e7025919a8df692d0ee142113ab1ea9ec
Trần Trung Hiếu <hieu.trantrung1204@icloud.com>
2018-12-19 15:43:53 +0700

    + fix Framework2D.exe SFML visual studio 2015 express for desktop

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,49 @@
+#include <math.h>
+#include <cstdlib>
+#include <vector>
+#include <ctime> 
+#include <iostream>
+
 #include "SFML/Graphics.hpp"
+
+using namespace sf;
+using namespace std;
+
+#define WINDOWS_W 900
+#define WINDOWS_H 600
+
 
 int main(int argc, char** argv)
 {
-	//sf::RenderWindow renderWindow(sf::VideoMode(640, 480), "Hello SFML");
+	srand((unsigned int)time(NULL));
+	RenderWindow window(VideoMode(WINDOWS_W, WINDOWS_H), "Spaceship Game!", Style::Default);
+	window.setFramerateLimit(200);
+
+	/////////////
+	sf::CircleShape circleShape(200);
+	circleShape.setFillColor(sf::Color::Blue);
+
+	Clock clock;
+	Time elapsed;
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		elapsed = clock.getElapsedTime();
+
+		clock.restart();
+
+		//Draw ===================================================================== DRAW
+		window.clear();
+		//SceneMgr::GetInstance()->Render( window);
+		window.draw(circleShape);
+
+		window.display();
+	}
 
 }
