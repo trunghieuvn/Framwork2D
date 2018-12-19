99dbf938b6ca4d01a0cd09cbb8f5a984fd9cfda4
Trần Trung Hiếu <hieu.trantrung1204@icloud.com>
2018-12-19 17:18:39 +0700

    + demo move object

--- source/main.cpp
+++ source/main.cpp
@@ -9,23 +9,27 @@
 using namespace sf;
 using namespace std;
 
-#define WINDOWS_W 900
-#define WINDOWS_H 600
+#define WINDOWS_W	900
+#define WINDOWS_H	600
+#define FPS_LIMIT	30
 
+#define PLAYER_SPEED	150
 
 int main(int argc, char** argv)
 {
 	srand((unsigned int)time(NULL));
 	RenderWindow window(VideoMode(WINDOWS_W, WINDOWS_H), "Spaceship Game!", Style::Default);
-	//window.setFramerateLimit(200);
+	window.setFramerateLimit(FPS_LIMIT);
 
 	// ---------------------- Draw Color ----------------------
-	sf::CircleShape circleShape(200);
+	sf::CircleShape circleShape(50);
 	circleShape.setFillColor(sf::Color::Blue);
 	// ----------------------Draw Image  ----------------------
 	sf::Texture texture;
 	texture.loadFromFile("resources/player.png");
-	sf::Sprite sprite(texture);
+
+	sf::Sprite player(texture);
+	int dir = 1;
 
 	Clock clock;
 	Time elapsed;
@@ -39,14 +43,33 @@ int main(int argc, char** argv)
 		}
 
 		elapsed = clock.getElapsedTime();
+		// A microsecond is 1/1,000,000th of a second, 1000 microseconds == 1 millisecond
+		float dt = clock.getElapsedTime().asMicroseconds() * 1.0f / 1000000;
+		//std::cout << "dt: " << dt << std::endl;
+		// Start the countdown over.  Think of laps on a stop watch.
 		clock.restart();
 
-		//Draw ===================================================================== 
-		window.clear();
-		
-		window.draw(circleShape);
-		window.draw(sprite);
+		// ================================ Update ================================ 
+		auto pos = player.getPosition();
+		if (pos.x > WINDOWS_W - texture.getSize().x)
+		{
+			dir = -1;
+		}
+		if (pos.x < 0)
+		{
+			dir = 1;
+		}
 
+		pos.x += PLAYER_SPEED * dt * dir;
+		std::cout << "x: " << pos.x << std::endl;
+
+		player.setPosition(pos);
+		
+		// ================================ Draw ================================ 
+		window.clear();
+		window.draw(circleShape);
+		window.draw(player);
+		
 		window.display();
 	}
 
